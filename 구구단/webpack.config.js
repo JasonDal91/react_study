463c58d76a25c990ecbdca6b5be73864328a5a0c
Jason <jasondal91@gmail.com>
2022-11-14 23:59:40 +0900

    study

--- 구구단/webpack.config.js
+++ 구구단/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
@@ -10,17 +11,30 @@ module.exports = {
     app: './client',
   },
   module: {
-    result: [
+    rules: [
       {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                // 자동으로 옛날 브라우저들 지원해주는 애이다.
+                targets: {
+                  browsers: ['> 5% in KR', 'last 2 chrome versions'], // 원하는 브라우저에만 맞춰서 설정 해줄 수도 있다.
+                  // 회사에서 지원하고자 하는 브라우저에만 맞추는게 중요, 안그럼 넘 많아서 느려진다. //browserlist에서 자세한 옵션들을 볼 수 있다.
+                },
+                debug: true, // 개발용에서 쓴다.
+              },
+            ],
+            '@babel/preset-react',
+          ],
         },
       },
     ],
   },
-
+  plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
   output: {
     filename: 'app.js',
     path: path.join(__dirname, 'dist'),
---
+++ 숫자야구/NumberBaseball.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+function getNumbers() {
+  // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
+}
+
+class NumberBaseball extends Component {
+  state = {
+    result: '',
+    value: '',
+    answer: getNumbers(),
+    tries: [],
+  };
+
+  onSubmit = () => {};
+
+  onChange = () => {};
+
+  render() {
+    return (
+      <>
+        <h1>{this.state.result}</h1>
+        <form onSubmit={this.onSubmitForm}>
+          <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
+        </form>
+        <div>시도: {this.state.tries.length}</div>
+        <ul>
+          {[
+            { fruit: '사과', taste: '맛없다' },
+            { fruit: '바나나', taste: '맛없다1' },
+            { fruit: '포도', taste: '맛없다2' },
+            { fruit: '귤', taste: '맛없다3' },
+            { fruit: '감', taste: '맛없다4' },
+            { fruit: '바나나', taste: '맛없다5' },
+          ].map((v) => {
+            return (
+              <li key={v.fruit + v.taste}>
+                <b>{v.fruit}</b>
+                {v.taste}
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default NumberBaseball;
