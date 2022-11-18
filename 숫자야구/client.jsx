5ab9b8d5763bdf8a1e192d11af7674a4bec1e182
Jason <jasondal91@gmail.com>
2022-11-18 23:47:11 +0900

    study: shouldComponent로 랜더링 최적화

--- 숫자야구/client.jsx
+++ 숫자야구/client.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom/client');
-const { default: NumberBaseball } = require('./NumberBaseball');
+const { default: Test } = require('./RenderTest');
+// const { default: NumberBaseball } = require('./NumberBaseball');
 
-ReactDOM.createRoot(document.querySelector('#root')).render(<NumberBaseball />);
+ReactDOM.createRoot(document.querySelector('#root')).render(<Test />);
