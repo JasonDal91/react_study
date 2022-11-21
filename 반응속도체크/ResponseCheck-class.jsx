3bd994823380968e1769712b38110193b7a891f8
Jason <jasondal91@gmail.com>
2022-11-21 23:17:01 +0900

    study: jsx에서 조건문 사용 및 state 조작 연습

--- 반응속도체크/ResponseCheck-class.jsx
+++ 반응속도체크/ResponseCheck-class.jsx
@@ -7,11 +7,58 @@ class ResponseCheck extends Component {
     result: [], // 평균 시간 구하려면 시간들을 state로 만들어 놔야 한다.
   };
 
-  onClickScreen = () => {};
+  timeout;
+  startTime;
+  endTime;
+
+  onClickScreen = () => {
+    const { state, message, result } = this.state;
+    if (state === 'waiting') {
+      this.setState({
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요.',
+      });
+      this.timeout = setTimeout(() => {
+        this.startTime = new Date(); // 초록색이 된 순간 시작해서 클릭할 때 까지가 반응 속도이다.
+        this.setState({
+          state: 'now',
+          message: '지금 클릭',
+        });
+      }, Math.floor(Math.random() * 1000) + 2000);
+    } else if (state === 'ready') {
+      // 성급하게 클릭
+      clearTimeout(this.timeout);
+      this.setState({
+        state: 'waiting',
+        message: '너무 성급하시군요! 초록색이 된 후에 클릭하세요.',
+      });
+    } else if (state === 'now') {
+      // 반응속도 체크
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          message: '클릭해서 시작하세요.',
+          result: [...prevState.result, this.endTime - this.startTime],
+        };
+      });
+    }
+  };
+
+  onReset = () => {
+    this.setState({
+      result: [],
+    });
+  };
 
   renderAverage = () => {
     const { result } = this.state;
-    return result.length === 0 ? null : <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>;
+    return result.length === 0 ? null : (
+      <>
+        <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+        <button onClick={this.onReset}>리셋</button>
+      </>
+    );
   };
 
   render() {
