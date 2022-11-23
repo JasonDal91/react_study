31d4590cb8beff0140096a0e45b527eb08b05a6d
Jason <jasondal91@gmail.com>
2022-11-23 23:40:36 +0900

    study: useRef

--- 반응속도체크/client.jsx
+++ 반응속도체크/client.jsx
@@ -1,5 +1,5 @@
 const React = require('react');
 const ReactDOM = require('react-dom/client');
-const { default: ResponseCheck } = require('./ResponseCheck-class');
+const { default: ResponseCheck } = require('./ResponseCheck');
 
 ReactDOM.createRoot(document.querySelector('#root')).render(<ResponseCheck />);
